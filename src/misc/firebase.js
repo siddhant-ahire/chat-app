dbd8ee0b70998d1a0d7d70f64c9538ff09b7c92c
Siddhant <siddhantahireofficial@gmail.com>
2020-11-24 17:04:21 +0530

    initalized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyDTAE9e5SCMK9Gfry1Ot-Q6KtOE7utE_SI",
+    authDomain: "chat-web-app-6b977.firebaseapp.com",
+    databaseURL: "https://chat-web-app-6b977.firebaseio.com",
+    projectId: "chat-web-app-6b977",
+    storageBucket: "chat-web-app-6b977.appspot.com",
+    messagingSenderId: "361034204823",
+    appId: "1:361034204823:web:8c9e2c1b6fd2422cf8cbd2"
+  };
+
+const app = firebase.initializeApp(config);
+

d05971dadb4bde209598aae10599dc001e8fe061
Siddhant <siddhantahireofficial@gmail.com>
2020-11-28 13:59:06 +0530

    sign in and sign out logic done and also learn about media queries docs

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import { Button, Drawer, Icon } from 'rsuite'
+import React, { useCallback } from 'react'
+import { Alert, Button, Drawer, Icon } from 'rsuite'
 import Dashboard from '.';
-import { useModalState } from '../../misc/custom-hooks'
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks'
+import { auth } from '../../misc/firebase';
 
 
 const DashboardToggle = () => {
 
     const { isOpen, close, open} = useModalState();
+    const isMobile = useMediaQuery('(max-width: 992px)');
+    const onSignOut = useCallback(()=>{
+        auth.signOut();
+        Alert.info('Signed Out',4000);
+        close();
+    },[close])
 
     return (
         <>
             <Button  block  color='blue' onClick={open}>
-                <Icon icon="facebook" /> Dashboard
+                <Icon icon="dashboard" /> Dashboard
             </Button>   
-            <Drawer show={isOpen} onHide={close} placement="left">
-                <Dashboard/>
+            <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
+                <Dashboard onSignOut={onSignOut} />
             </Drawer>
         </>
     )
--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,4 @@
-import { useState, useCallback} from 'react';
+import { useState, useCallback, useEffect} from 'react';
 
 
 export function useModalState(defaultValue = false){
@@ -11,5 +11,25 @@ export function useModalState(defaultValue = false){
     
 } 
 
+// usage
+// const is992px = useMediaQuery('(max-width: 992px)')
 
+export const useMediaQuery = query => {
+    const [matches, setMatches] = useState(
+      () => window.matchMedia(query).matches
+    );
+  
+    useEffect(() => {
+      const queryList = window.matchMedia(query);
+      setMatches(queryList.matches);
+  
+      const listener = evt => setMatches(evt.matches);
+  
+      queryList.addListener(listener);
+      return () => queryList.removeListener(listener);
+    }, [query]);
+  
+    return matches;
+  };
+  
 

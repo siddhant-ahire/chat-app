0891b929b2113ae8589155598c42206e42a9a690
Siddhant <siddhantahireofficial@gmail.com>
2020-12-10 15:44:07 +0530

    displayed chat inside room

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { database } from '../../../misc/firebase';
+import { transformToArrWithId } from '../../../misc/helpers';
+import MessageItem from './MessageItem';
 
 const Messages = () => {
+
+    const {chatId} = useParams();
+    // eslint-disable-next-line no-unused-vars
+    const [messages, setMessages] = useState(null);
+    const isChatEmpty = messages && messages.length === 0;
+    const canShowMessages = messages && messages.length > 0;
+
+    useEffect(()=>{
+        const messagesRef = database.ref('/messages');
+
+        messagesRef.orderByChild('roomId').equalTo(chatId).on('value', (snap)=>{
+
+            const data = transformToArrWithId(snap.val())
+            setMessages(data);
+        })
+        return ()=>{
+            messagesRef.off('value');
+        }
+    },[setMessages,chatId])
+
     return (
-        <div>
-            Messages
-        </div>
+        <ul className="msg-list custom-scroll">
+            {isChatEmpty && <li>No Messages yet</li>}
+            {canShowMessages && messages.map(msg => <MessageItem key={msg.id} message={msg}/>)}
+        </ul>
     )
 }
 
